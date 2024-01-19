8475acd418e89d1eef862b7f9a016d6c2e5bff8e
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2024-01-19 09:24:40 -0300

    [IMPROVEMENT] - binary-streaming - master

--- test.ts
+++ test.ts
@@ -10,11 +10,11 @@ const textMessageInBinary = "1000111 1100001 1100010 1110010 1101001 1100101 110
 
 const messageInBinary = binaryStreaming.textToBinary(textMessage);
 log("Test: [textToBinary] - status: ", messageInBinary===textMessageInBinary ? "Ok" :"Failed")
-const messageInText = binaryStreaming.binaryToText(messageInBinary);
-log("Test: [binaryToText] - status: ", messageInText===textMessage ? "Ok" :"Failed")
+log("Test: [binaryToText] - status: ", binaryStreaming.binaryToText(messageInBinary)===textMessage ? "Ok" :"Failed")
 
 log("Test: [decimalToBinary] - status(1): ", binaryStreaming.decimalToBinary(10)==="1010" ? "Ok" :"Failed")
 log("Test: [decimalToBinary] - status(2): ", binaryStreaming.decimalToBinary(9)==="1001" ? "Ok" :"Failed")
 log("Test: [decimalToBinary] - status(3): ", binaryStreaming.decimalToBinary(26)==="11010" ? "Ok" :"Failed")
+log("Test: [decimalToBinary] - status(4): ", binaryStreaming.decimalToBinary(37)==="100101" ? "Ok" :"Failed")
 
 log("[BinaryStreaming] - Test finished!")
